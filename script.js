0dd18ca9c632005ed67a6de72a37ced0fc6d030d
anna <kstars.anna@gmail.com>
2021-11-28 23:03:13 +0530

    success

---
+++ script.js
@@ -0,0 +1,19 @@
+// function displayValue(num){
+//      result.value+=num
+// }
+var displayValue=(num)=>result.value+=num
+// function clearBox(){
+//     result.value=" "
+// }
+var clearBox=()=>result.value=""
+// function evaluateExpression(){
+//     result.value=eval(result.value)
+// }
+var  evaluateExpression=()=>result.value=eval(result.value)
+
+// function backSpace(){
+// let res=document.querySelector("#result")
+// let cur_val=res.value
+// res.value=cur_val.slice(0,-1)
+// }
+var backSpace=()=>result.value=result.value.slice(0,-1)
